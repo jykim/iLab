4db1432d1594120629b01058c701c50dda2e1fa8
lifidea <lifidea@csdhcp205.csdhcp.cs.local>
2009-07-17 11:19:09 -0400

    Merge Type :multiply added

--- lib/object/result_document_set.rb
+++ lib/object/result_document_set.rb
@@ -124,10 +124,18 @@ class ResultDocumentSet < DocumentSet
           next
         end
         #debugger
-        score_doc = Math.exp(score_raw)
-        score_col = score_doc * (col_score[d.qid][qs[:col_type]]) 
-        docs[d.qid][d.did].score = (score_col * col_weight + score_doc) / (1 + col_weight)
-        info "[create_by_merge] #{docs[d.qid][d.did].score.r3} = #{score_col.r3} * #{col_weight} + #{score_doc.r3} (#{d.did})" if d.qid == 1 && d.rank <= 3
+        case (o[:merge_type] || :cori)
+        when :cori
+          score_doc = Math.exp(score_raw)
+          score_col = score_doc * (col_score[d.qid][qs[:col_type]]) 
+          docs[d.qid][d.did].score = (score_col * col_weight + score_doc) / (1 + col_weight)
+          info "[create_by_merge] #{docs[d.qid][d.did].score.r3} = #{score_col.r3} * #{col_weight} + #{score_doc.r3} (#{d.did})" if d.qid == 1 && d.rank <= 3
+        when :multiply
+          score_doc = Math.exp(score_raw)
+          score_col = score_doc * (col_score[d.qid][qs[:col_type]]) 
+          docs[d.qid][d.did].score = (score_col * score_doc)
+          info "[create_by_merge] #{docs[d.qid][d.did].score.r3} = #{score_col.r3} * #{score_doc.r3} (#{d.did})" if d.qid == 1 && d.rank <= 3
+        end
       end#doc
     end#docset
 
