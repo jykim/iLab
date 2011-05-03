d974fa9b260a058f873d498fe5bba9c773dfdc2c
Jinyoung Kim <jykim@lifidea.local>
2011-05-03 12:13:24 -0400

    !

--- app/logs/prms_logs.rb
+++ app/logs/prms_logs.rb
@@ -122,7 +122,7 @@ $ scp qrel/qrel_qlm_train $SYH/work/prj/dih/imdb/qrels.train
 
 # Mixture MP Debugging (5/2)
 
-$o = {:redo=>true, :topic_id=>'test', :verbose=>:mp}; $method='prms_mix'; $col='enron'; $exp='perf'; $remark='0503'; eval IO.read('run_prms.rb')
+$o = {:topic_id=>'test', :verbose=>:mp}; $method='prms_mix'; $col='trec'; $exp='perf'; $remark='0503'; eval IO.read('run_prms.rb')
 
 $o = {:verbose=>:mp, :topic_id=>'test'}; $method='prms_bgram'; $col='trec'; $exp='perf'; $remark='0502'; eval IO.read('run_prms.rb')
 
