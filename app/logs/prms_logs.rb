b4edef746fdce068b05590dd396bab53e6a224ff
Jinyoung Kim <jykim@csdhcp023.csdhcp.cs.local>
2011-05-06 15:41:50 -0400

    !

--- app/logs/prms_logs.rb
+++ app/logs/prms_logs.rb
@@ -7,3 +7,5 @@ $o = {:topic_id=>'test', :verbose=>:mp, :redo=>true}; $method='prms_mix'; $col='
 $o = {:topic_id=>'test', :verbose=>:mp, :redo=>true, :sparam=>get_sparam('jm',0.1) }; $method='prms_mix'; $col='trec'; $exp='perf'; $remark='0505_jm_optmap'; eval IO.read('run_prms.rb')
 
 $o={:mode=>:mix_weights,:opt_for=>'map',:redo=>true,:topic_id=>'train', :sparam=>get_sparam('jm',0.1)}; $col='trec' ;$exp='optimize_rpm'; $method='golden'; $remark='0506_jm'; eval IO.read('run_prms.rb')
+
+$o={:mode=>:hlm_weights,:topic_id=>'train'}; $col='enron' ;$exp='optimize_prm'; $method='golden'; eval IO.read('run_prms.rb')
