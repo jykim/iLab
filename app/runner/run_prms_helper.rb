c5ce8edb8be36f5d48388a3db1f970d8360ae494
Jinyoung Kim <jykim@lifidea.local>
2011-05-14 17:35:42 -0400

    !

--- app/runner/run_prms_helper.rb
+++ app/runner/run_prms_helper.rb
@@ -118,10 +118,32 @@ def init_collection(col)
     $i.config_path( :work_path=>File.join($exp_root,col) ,:index_path=>$index_path )
     puts "work_path : #$work_path"
     $ptn_qry_title = /\<title\> (.*) \<\/title\>/
-    $offset = 201
     $fields =  ['subject','from','to','date','body']
     if !File.exist?($index_path)
       $engine.build_index($col_id , "#$exp_root/enron/raw_doc" , $index_path , :fields=>$fields, :stemmer=>:krovetz, :stopword=>false)
+    end
+    case $o[:topic_id]
+    when 'all'
+      $offset, $count = 1, 214
+      $file_topic ,$file_qrel = 'queries.all' , 'qrels.all'
+    when 'test'
+      $offset, $count = 1, 150
+      $file_topic ,$file_qrel = 'queries.test' , 'qrels.test'
+    when 'train'
+      $offset, $count = 151, 64
+      $file_topic ,$file_qrel = 'queries.train' , 'qrels.train'
+    end
+    $sparam = get_sparam('jm',0.1)
+    $title_field = "SUBJECT"
+    
+  when 'enron2'
+    $index_path = "#$exp_root/enron2/index_enron"
+    $i.config_path( :work_path=>File.join($exp_root,col) ,:index_path=>$index_path )
+    puts "work_path : #$work_path"
+    $ptn_qry_title = /\<title\> (.*) \<\/title\>/
+    $fields =  ['subject','person','date','body']
+    if !File.exist?($index_path)
+      $engine.build_index($col_id , "#$exp_root/enron2/raw_doc" , $index_path , :fields=>$fields, :stemmer=>:krovetz, :stopword=>false)
     end
     case $o[:topic_id]
     when 'all'
@@ -223,6 +245,15 @@ def set_collection_param(col_id)
     $hlm_weight = [0.674, 0.562, 0.562, 0.146, 0.472]
     $prmd_lambda = 0.7
 
+  when 'enron2'
+    $sparam = get_sparam('jm',0.1)#
+    $sparam_prm = get_sparam('jm',0.1)#
+    $sparam_mflm = get_sparam('jm',0.1)#
+    
+    $mix_weights = [0.154, 1.0, 0.01, 0.01, 0.299]	
+    $hlm_weight = [0.674, 0.562, 0.562, 0.146, 0.472]
+    $prmd_lambda = 0.7
+
   when 'imdb'
     $sparam = get_sparam('dirichlet',1000)
     $sparam_prm = get_sparam('jm',0.1)#get_sparam('dirichlet',250)
@@ -230,9 +261,9 @@ def set_collection_param(col_id)
     
     case $o[:topic_id]
     when "dcv1"
-      
+      $mix_weights = [0.766, 0.422, 1.0, 0.01, 1.0]	# (cos)
     when "dcv2"
-      
+      $mix_weights = [0.333, 1.0, 0.299, 0.01, 0.01]	# (cos)
     else
       $mix_weights = [0.388, 0.01, 1.0, 0.388, 1.0]
     end
