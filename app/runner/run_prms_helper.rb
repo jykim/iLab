8869e34918b80a5147aeb2d8ff81fed4229016cd
Jinyoung Kim <jykim@csdhcp045.csdhcp.cs.local>
2011-11-29 14:12:30 -0500

    !

--- app/runner/run_prms_helper.rb
+++ app/runner/run_prms_helper.rb
@@ -128,28 +128,23 @@ def init_collection(col)
     # Get Rdoc list (needed for oracle MP calculation)
     
   when 'facebook'
-    $index_path = "#$exp_root/facebook/index_facebook"
+    $index_path = "#$exp_root/facebook/index_fb"
     $i.config_path( :work_path=>File.join($exp_root,col) ,:index_path=>$index_path )
     puts "work_path : #$work_path"
-    $ptn_qry_title = /\<title\>\s(.*)\s\<\/title\>/
-    $fields =  ['subject','from','to','date','body']
+    $ptn_qry_title = /\[0-9]+\s(.*)/
+    $fields =  ["username", "message", "postctime", "postutime", "cmtname", "cmtmessage", "cmttime", "likename", "link", "description", "caption", "olink", "photoname", "albumname", "type", "count", "likes"]
     if !File.exist?($index_path)
-      $engine.build_index($col_id , "#$exp_root/enron/raw_doc" , $index_path , :fields=>$fields, :stemmer=>:krovetz, :stopword=>false)
+      $engine.build_index($col_id , "#$exp_root/facebook/Facebook-541120474-xmldoc" , $index_path , :fields=>$fields, :stemmer=>false, :stopword=>false)
     end
     case $o[:topic_id]
-    when 'all'
-      $offset, $count = 1, 214
-      $file_topic ,$file_qrel = 'queries.all' , 'qrels.all'
-    when 'test'
-      $offset, $count = 1, 150
-      $file_topic ,$file_qrel = 'queries.test' , 'qrels.test'
-    when 'train'
-      $offset, $count = 151, 64
-      $file_topic ,$file_qrel = 'queries.train' , 'qrels.train'
+    when 'fb2'
+      $offset, $count = 1, 36
+      $file_topic ,$file_qrel = 'fbuser2.qry' , 'fbuser2.qrel'
     end
     $sparam = get_sparam('jm',0.1)
-    $title_field = "SUBJECT"
-
+    $title_field = "message"
+  when 'twitter'
+    $fields= ["cur_text", "cur_user", "cur_replyto", "cur_src", "cur_time", "olink", "re_text", "re_user", "re_replyto", "re_src", "re_time"]
   when 'enron'
     $index_path = "#$exp_root/enron/index_enron"
     $i.config_path( :work_path=>File.join($exp_root,col) ,:index_path=>$index_path )
