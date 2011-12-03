19416a7ae0de07a7ae1d7d29afce1ad1bc2a3bd8
Jinyoung Kim <jykim@jykim-mbp.local>
2011-12-03 18:08:40 -0500

    !

--- app/logs/log_prms_frm.rb
+++ app/logs/log_prms_frm.rb
@@ -1,11 +1,11 @@
-$o = {:verbose=>true, :topic_id=>'fb2'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'fb2'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
 
-$o = {:verbose=>true, :topic_id=>'fb3'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'fb3'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
 
-$o = {:verbose=>true, :topic_id=>'fb4'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'fb4'}; $method='param_smt'; $col='facebook'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
 
-$o = {:verbose=>true, :topic_id=>'tw1'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'tw1'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
 
-$o = {:verbose=>true, :topic_id=>'tw5'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'tw5'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
 
-$o = {:verbose=>true, :topic_id=>'tw6'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202'; eval IO.read('run_prms.rb')
+$o = {:verbose=>true, :topic_id=>'tw6'}; $method='param_smt'; $col='twitter'; $exp='perf'; $remark='1202_CJ'; eval IO.read('run_prms.rb')
