7e4220256087dcd29081d2857852e9bf534a53c8
Jinyoung Kim <jykim@jykim-mbp.local>
2012-01-15 17:32:15 -0500

    Twir Feature extraction

---
+++ app/adhoc/twir_feature.rb
@@ -0,0 +1,68 @@
+load 'app/ilab.rb'
+require 'nokogiri'
+
+def parse_doc(filename)
+  nk = Nokogiri::HTML(IO.read(filename))
+  nk.xpath("//div[@id='vt-header']").map{|e|e.parent = nk.at_css "html"}
+  text = nk.xpath("//div[@id='toadjaw-article']").map{|e|e.text()}.join("\n")
+  title = nk.xpath("//h1[@id='vt-title']").first.text()
+  url = nk.xpath("//div/a[text()='Original']").first['href']
+  {:title=>title, :text=>text, :url=>url}
+end
+
+def index_doc(id, did, doc_hash)
+  IR::Document.new(id, did, doc_hash.map_hash{|k,v|[k, LanguageModel.new(v)]})
+end
+
+
+def index_path(path)
+  docs = {}
+  Dir.entries(path).each_with_index do |fn,i| 
+    next if ['.','..'].include?(fn)
+    #puts fn
+    docs[File.basename(fn, ".html")] = index_doc(i, File.basename(fn, ".html"), parse_doc(File.join(path,fn)))
+  end
+  docs
+end
+
+def calc_overlap(qwords, dlm, o = {})
+  scores = qwords.map do |q|
+    dlm[q] ? 1 : 0
+  end
+  scores.sum / qwords.size.to_f
+end
+
+def parse_query(query)
+  query.split(/\s+/).map{|e|e.stem}
+end
+
+def process_input(file, ofile = nil)
+  File.open( file ) do |f|
+    File.open( ofile || file+'.out', 'w' ) do |of|
+      while line = f.gets
+        begin
+          e = line.split("\t")
+          qid, query, tid, url_hash, tweet = e[0], e[1], e[3], e[7], e[12]
+          outrow = [qid, tid, url_hash]
+          doc = $docs[url_hash]
+          next if !doc
+          qwords, twords = parse_query(query), parse_query(tweet)
+          outrow.concat doc.flm.map{|k,v| calc_overlap(qwords,v.f).r3 }
+          outrow.concat doc.flm.map{|k,v| calc_overlap(twords,v.f).r3 }
+          of.puts outrow.join("\t")
+        rescue Exception => e
+          puts "Error in [#{line}]",e
+        end
+        #sleep(1)
+      end
+    end
+  end
+end
+
+=begin
+$docs = index_path('html')
+
+process_input('TcUrl.list.resolve.10')
+=end
+
+#dt = IO.read('TcUrl.list.resolve.10').split("\n").map{|e|e.split("\t")}
